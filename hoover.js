30aa3e87b9e0a5a1fc44f9cf92178b3902124b2b
Mario90 <mgpinan1990@gmail.com>
2019-12-10 09:58:57 +0000

    Able to read file from .txt

--- hoover.js
+++ hoover.js
@@ -1,8 +1,14 @@
+var fs = require('fs');
+
+const text = fs.readFileSync("input.txt").toString().split("/n");
+
+console.log(text)
+
+
 class Hoover {
     constructor() {
         this.x = 1
         this.y = 2
     }
 }
-
 module.exports = Hoover;
